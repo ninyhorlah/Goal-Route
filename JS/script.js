559737b154a7795cc8361c3c52749e1065897b93
ninyhorlah <58943705+ninyhorlah@users.noreply.github.com>
2020-07-16 16:58:59 +0100

    updated

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    $('#slideshow .slick').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        prevArrow: '.prevArrow',
+        nextArrow: '.nextArrow',
+    });
+});
+
